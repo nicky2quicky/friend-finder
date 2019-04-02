35dd47f9c7e370d89de8ce557b8d9c848e020fdf
nicky2quicky <NICKTOM22@GMAIL.COM>
2019-04-02 16:58:25 -0700

    added friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friends = [{
+    name: "Frylock",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/c/cd/AthfFrylock2.png/revision/latest?cb=20101230022228",
+    scores: ['1', '1', '1', '1','1','1','1','1','1','1']
+},
+{
+    name: "Master Shake",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/7/71/Shake.png/revision/latest?cb=20111113195407",
+    scores: ['4', '4', '4', '4','4','4','4','4','4','4']
+},
+{
+   name: "Meatwad",
+   photo: "https://vignette.wikia.nocookie.net/athfwiki/images/a/a8/Meatwad.jpg/revision/latest?cb=20110111234925",
+   scores: ['3', '3', '3', '3','3','3','3','3','3','3']
+},
+{
+    name: "Carl Brutananadilewski",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/7/71/Carl.png/revision/latest?cb=20140108162838",
+    scores: ['5', '5', '5', '5','5','5','5','5','5','5']
+ },
+ {
+    name: "Ignignokt",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/e/eb/Ignignokt.png/revision/latest?cb=20080316001408",
+    scores: ['4', '5', '2', '3','4','2','1','3','4','5']
+ },
+ {
+    name: "Err",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/2/2a/Cartoons_ATHF-err.gif/revision/latest?cb=20080701182004",
+    scores: ['1', '2', '4', '5','4','2','3','3','5','5']
+ },
+ {
+    name: "Emory",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/1/17/Emory.gif/revision/latest?cb=20080815005336",
+    scores: ['5', '5', '3', '5','4','1','3','1','5','5']
+ },
+{
+ name: "Ogelthorpe",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/8/81/Oglethorpe.png/revision/latest/scale-to-width-down/250?cb=20150703194941",
+    scores: ['5', '5', '3', '5','4','1','3','1','5','5']
+ },
+{
+ name: "Cybernetic Ghost of Christmas Past from the Future",
+    photo: "https://vignette.wikia.nocookie.net/athfwiki/images/5/53/Cybernetic_Ghost.png/revision/latest?cb=20110214013837",
+    scores: ['5', '5', '3', '5','4','1','3','1','5','5']
+ },
+
+];
+
+module.exports = friends;
+
